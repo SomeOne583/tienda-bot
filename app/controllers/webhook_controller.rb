1b9212a8a4d40cd970d304525d483b59fb5470a9
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-16 19:18:48 -0600

    Testing answer message

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,6 +3,10 @@ class WebhookController < ApplicationController
 
     def index
         user_id = params[:message][:from][:id]
+        puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+        puts params[:message][:from][:id]
+        puts params[:message][:text]
+        puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
         response = Faraday.post("https://api.telegram.org/bot#{ENV['TIENDA_TOKEN']}/sendMessage", {"chat_id": user_id, "text": params[:message][:text]}, "Content-Type" => "application/json")
         puts response
     end
