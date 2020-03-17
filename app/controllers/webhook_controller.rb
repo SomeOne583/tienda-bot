e4873281299b66157c176478febab6ee58cd8590
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-16 20:07:23 -0600

    Testing answer message

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,9 +1,10 @@
 class WebhookController < ApplicationController
     # https://api.telegram.org/bot#{ENV['TIENDA_TOKEN']}/METHOD_NAME
+    updater = WeebhookFunctions.new
     
     def index
         user_id = params[:message][:from][:id]
         message = params[:message][:text]
-        send_message = UpdaterTest::send_message(user_id, message)
+        send_message = updater.send_message(user_id, message)
     end
 end
