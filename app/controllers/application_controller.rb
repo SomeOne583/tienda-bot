c5b47767081bd94d92c992a21cd234d1b2642360
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-17 11:35:10 -0600

    Test answer with params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
-    response = Faraday.post("https://api.telegram.org/bot#{ENV['TIENDA_TOKEN']}/setWebhook", "url=https://tienda-bot.herokuapp.com/#{ENV['TIENDA_TOKEN']}")
+    response = Faraday.post(
+        "https://api.telegram.org/bot#{ENV['TIENDA_TOKEN']}/setWebhook",
+        "url=https://tienda-bot.herokuapp.com/#{ENV['TIENDA_TOKEN']}"
+    )
     puts response.body
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :receipts
-  resources :suppliers
-  resources :appointments
-  resources :clients
-  resources :employees
+  # resources :receipts
+  # resources :suppliers
+  # resources :appointments
+  # resources :clients
+  # resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "#{ENV['TIENDA_TOKEN']}", to: "webhook#index"
   post "#{ENV['TIENDA_TOKEN']}", to: "webhook#index"
