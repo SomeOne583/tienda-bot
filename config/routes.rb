0e149b0482e1ff05e3d0c1c72fbc68039755444f
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-17 12:41:55 -0600

    Typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :receipts
   # resources :suppliers
   # resources :appointments
-  # resources :clients
+  resources :clients
   # resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "#{ENV['TIENDA_TOKEN']}", to: "webhook#index"
