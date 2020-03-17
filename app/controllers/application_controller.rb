c3e653e745026915d6b4da40edc82d41d3e36fd0
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-16 18:51:27 -0600

    Testing webhook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API
-    response = Faraday.post("https://api.telegram.org/bot#{ENV['TIENDA_TOKEN']}/setWebhook", "url=https://tienda-bot.herokuapp.com/#{ENV['TIENDATOKEN']}10")
+    response = Faraday.post("https://api.telegram.org/bot#{ENV['TIENDA_TOKEN']}/setWebhook", "url=https://tienda-bot.herokuapp.com/#{ENV['TIENDA_TOKEN']}")
     puts response.body
 end
