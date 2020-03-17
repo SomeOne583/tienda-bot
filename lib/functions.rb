23eaf02da1d45d35b7d89845216ae83b34b02ac3
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-17 14:51:18 -0600

    Trying validation

--- lib/functions.rb
+++ lib/functions.rb
@@ -12,7 +12,7 @@ class Functions
                     puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
                     puts user_to_validate
                     puts a.split(':')[1].gsub(/"/, '')
-                    puts a.split(':')[1].gsub(/"/, '') == user_to_validate
+                    puts a.split(':')[1].gsub(/"/, '').to_s == user_to_validate
                     puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
                     return true if a.split(':')[1].gsub(/"/, '') == user_to_validate
                 end
